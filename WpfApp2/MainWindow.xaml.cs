399589a4f6b5503c357fc16354a2b30557578b5e
kot04ka <50462393+kot04ka@users.noreply.github.com>
2024-06-16 21:26:41 +0300

    new design
    изменение в дизайне и переработка интерфеса

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,61 +1,162 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
-using Newtonsoft.Json;
+using System.Windows.Controls;
 
-namespace WpfApp2
+namespace AnimeApp
 {
 	public partial class MainWindow : Window
 	{
+		private int currentPage = 1;
+		private const int itemsPerPage = 10;
+
 		public MainWindow()
 		{
 			InitializeComponent();
-			LoadAnimeData();
+			LoadAnime();
 		}
 
-		private async void LoadAnimeData()
+		private async void LoadAnime()
 		{
-			var animeList = await FetchAnimeData();
-			AnimeListView.ItemsSource = animeList;
-		}
-
-		private async Task<List<Anime>> FetchAnimeData()
-		{
-			using (HttpClient client = new HttpClient())
+			try
 			{
-				client.BaseAddress = new Uri("https://api.anilibria.tv/v2/");
-				HttpResponseMessage response = await client.GetAsync("getTitles?filter=id,names,russian,description,poster");
+				var animeList = await GetAnimeList(currentPage);
+				AnimeListView.ItemsSource = animeList;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Error loading anime list: {ex.Message}");
+			}
+		}
 
-				if (response.IsSuccessStatusCode)
+		private async Task<List<Anime>> GetAnimeList(int page)
+		{
+			string url = "https://graphql.anilist.co";
+			string query = @"
+                query ($page: Int, $perPage: Int) {
+                  Page(page: $page, perPage: $perPage) {
+                    media(type: ANIME) {
+                      id
+                      title {
+                        romaji
+                      }
+                      description
+                      coverImage {
+                        large
+                      }
+                      streamingEpisodes {
+                        title
+                        url
+                      }
+                    }
+                  }
+                }";
+
+			var variables = new
+			{
+				page = page,
+				perPage = itemsPerPage
+			};
+
+			using (var client = new HttpClient())
+			{
+				var jsonContent = new StringContent(
+					new JObject
+					{
+						["query"] = query,
+						["variables"] = JObject.FromObject(variables)
+					}.ToString(),
+					System.Text.Encoding.UTF8, "application/json");
+
+				var response = await client.PostAsync(url, jsonContent);
+				var responseString = await response.Content.ReadAsStringAsync();
+				var data = JObject.Parse(responseString)["data"]["Page"]["media"];
+
+				var animeList = new List<Anime>();
+				foreach (var item in data)
 				{
-					string jsonResponse = await response.Content.ReadAsStringAsync();
-					var animeList = JsonConvert.DeserializeObject<List<Anime>>(jsonResponse);
-					return animeList;
+					var streamingEpisodes = new List<StreamingEpisode>();
+					foreach (var episode in item["streamingEpisodes"])
+					{
+						streamingEpisodes.Add(new StreamingEpisode
+						{
+							Title = episode["title"].ToString(),
+							Url = episode["url"].ToString()
+						});
+					}
+
+					animeList.Add(new Anime
+					{
+						Title = item["title"]["romaji"].ToString(),
+						Description = item["description"].ToString(),
+						Image = item["coverImage"]["large"].ToString(),
+						Episodes = streamingEpisodes
+					});
 				}
-				else
+				return animeList;
+			}
+		}
+
+		private void AnimeListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (AnimeListView.SelectedItem is Anime selectedAnime)
+			{
+				AnimeTitle.Text = selectedAnime.Title;
+				AnimeDescription.Text = selectedAnime.Description;
+				AnimeImage.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(selectedAnime.Image));
+				EpisodesListBox.ItemsSource = selectedAnime.Episodes;
+			}
+		}
+
+		private void EpisodesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (EpisodesListBox.SelectedItem is StreamingEpisode selectedEpisode)
+			{
+				try
 				{
-					MessageBox.Show("Ошибка при загрузке данных.");
-					return new List<Anime>();
+					var tabItem = ((TabControl)((Grid)((StackPanel)sender).Parent).Parent).SelectedItem as TabItem;
+					var mediaElement = tabItem.Header.ToString() == "Плеер" ? AnimePlayerFull : AnimePlayer;
+
+					mediaElement.Source = new Uri(selectedEpisode.Url);
+					mediaElement.Play();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"Error playing episode: {ex.Message}");
 				}
 			}
+		}
+
+		private void PreviousPage_Click(object sender, RoutedEventArgs e)
+		{
+			if (currentPage > 1)
+			{
+				currentPage--;
+				LoadAnime();
+			}
+		}
+
+		private void NextPage_Click(object sender, RoutedEventArgs e)
+		{
+			currentPage++;
+			LoadAnime();
 		}
 	}
 
 	public class Anime
 	{
-		public int Id { get; set; }
-		public string Russian { get; set; }
+		public string Title { get; set; }
 		public string Description { get; set; }
-		public Poster Poster { get; set; }
-
-		public string Name => Russian;
-		public string ImageUrl => Poster?.Url;
+		public string Image { get; set; }
+		public List<StreamingEpisode> Episodes { get; set; }
 	}
 
-	public class Poster
+	public class StreamingEpisode
 	{
+		public string Title { get; set; }
 		public string Url { get; set; }
 	}
 }
